0a09ee089bd69d82ce3820d76ab4f18c40c6d6df
oJaciel <jacielhbarichellO@gmail.com>
2025-07-21 20:53:24 -0300

    Modelo + Provider
    Criado modelo de dados e provider para a criação das cartas.
    
    Criado arquivo Utils para armazenar os ícones e cores disponíveis.

---
+++ lib/models/game_provider.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:memory_game/models/memory_card.dart';
+import 'package:memory_game/utils/game_utils.dart';
+
+class GameProvider {
+  final List<IconData> iconsList = GameUtils().iconsList;
+
+  final List<Color> colorsList = GameUtils().colorsList;
+
+  IconData getRandomIcon() {
+    return iconsList[Random().nextInt(iconsList.length)];
+  }
+
+  Color getRandomColor() {
+    return colorsList[Random().nextInt(colorsList.length)];
+  }
+
+  MemoryCard createCard() {
+    return MemoryCard(
+      id: Random().nextDouble().toString(),
+      icon: Icon(getRandomIcon()),
+      color: getRandomColor(),
+    );
+  }
+
+  List<MemoryCard> createGameCards() {
+    List<MemoryCard> list = [];
+    for (var i = 0; i < 8; i++) {
+      list.add(createCard());
+    }
+
+    return list;
+  }
+}
---
+++ lib/models/memory_card.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class MemoryCard {
+  String id;
+  Icon icon;
+  Color color;
+
+  MemoryCard({required this.id, required this.icon, required this.color});
+}
